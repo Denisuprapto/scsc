928c208a963602a64236a6273c7d474fa70ff790
Denisuprapto <denisuprapto@upi.edu>
2022-01-14 13:44:43 +0800

    scsc

--- editizin.sh
+++
@@ -1,9 +0,0 @@
-IZIN=$( curl https://raw.githubusercontent.com/Denisuprapto/daftarip/main/ipregister | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-rm -f setup.sh
-exit 0
-fi
